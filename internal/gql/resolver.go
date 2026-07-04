c13b3372234e80f8f81763cd14a6262f83a62fa4
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-07-04 09:23:53 -0400

    Basic gql setup

---
+++ internal/gql/resolver.go
@@ -0,0 +1,12 @@
+package gql
+
+import "make-backend/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require
+// here.
+
+type Resolver struct {
+	Store *database.Store
+}
