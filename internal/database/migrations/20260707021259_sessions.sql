d8b74f18a77dc12cee3ea113827b125ed7f818ab
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-07-06 22:48:00 -0400

    Added back scs sessions

---
+++ internal/database/migrations/20260707021259_sessions.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE sessions (
+	token TEXT PRIMARY KEY,
+	data BYTEA NOT NULL,
+	expiry TIMESTAMPTZ NOT NULL
+);
+
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+CREATE TABLE native_accounts (
+    user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    password_hash VARCHAR(60) NOT NULL
+)
+
+-- +goose Down
+DROP TABLE IF EXISTS native_accounts;
+DROP INDEX IF EXISTS sessions_expiry_idx;
+DROP TABLE IF EXISTS sessions; 
