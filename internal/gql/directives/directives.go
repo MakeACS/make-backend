5c691f4da30433544ad9231f280b91919fbb15e1
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-07-07 20:43:22 -0400

    rewrote all the directives to be more reusable bc I didn't like how I did it the first time

--- internal/gql/directives/directives.go
+++ internal/gql/directives/directives.go
@@ -9,89 +9,67 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 )
 
-func isAdmin(store *database.Store) func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-	return func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-		user_id, ok := ctx.Value("user_id").(int)
+func isAdmin(store *database.Store, ctx context.Context) (bool, error) {
+	user_id, ok := ctx.Value("user_id").(int)
 
-		if !ok {
-			return nil, fmt.Errorf("Access denied")
-		}
-
-		user, err := store.Users.GetUserById(ctx, user_id)
-		if err != nil {
-			return nil, err
-		}
-
-		return user.Admin, nil
+	if !ok {
+		return false, fmt.Errorf("Access denied")
 	}
+
+	user, err := store.Users.GetUserById(ctx, user_id)
+	if err != nil {
+		return false, err
+	}
+
+	return user.Admin, nil
 }
 
-func isManager(store *database.Store) func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-	return func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-		user_id, ok := ctx.Value("user_id").(int)
+func isManager(store *database.Store, ctx context.Context) (bool, error) {
+	user_id, ok := ctx.Value("user_id").(int)
 
-		if !ok {
-			return nil, fmt.Errorf("Access denied")
-		}
-
-		isManager, err := store.Users.IsManager(ctx, user_id)
-		if err != nil {
-			return nil, err
-		}
-
-		return isManager, nil
+	if !ok {
+		return false, fmt.Errorf("Failed to get user_id from context")
 	}
+
+	manager, err := store.Users.IsManager(ctx, user_id)
+	if err != nil {
+		return false, err
+	}
+
+	return manager, nil
 }
 
-func evalStaff(store *database.Store, ctx context.Context) (bool, error) {
+func isStaff(store *database.Store, ctx context.Context) (bool, error) {
 	user_id, ok := ctx.Value("user_id").(int)
 
 	if !ok {
 		return false, fmt.Errorf("Could not extract user_id from context")
 	}
 
-	isStaff, err := store.Users.IsStaff(ctx, user_id)
+	staff, err := store.Users.IsStaff(ctx, user_id)
 	if err != nil {
 		return false, err
 	}
 
-	return isStaff, nil
+	return staff, nil
 }
 
-func isStaff(store *database.Store) func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-	return func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+func isTrainer(store *database.Store, ctx context.Context) (bool, error) {
+	user_id, ok := ctx.Value("user_id").(int)
 
-		staff, err := evalStaff(store, ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		if staff {
-			return next(ctx)
-		} else {
-			return nil, fmt.Errorf("Unauthorized")
-		}
+	if !ok {
+		return false, fmt.Errorf("Failed to get user_id from context")
 	}
-}
 
-func isTrainer(store *database.Store) func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-	return func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-		user_id, ok := ctx.Value("user_id").(int)
-
-		if !ok {
-			return nil, fmt.Errorf("Access denied")
-		}
-
-		isTrainer, err := store.Users.IsTrainer(ctx, user_id)
-		if err != nil {
-			return nil, err
-		}
-
-		return isTrainer, nil
+	trainer, err := store.Users.IsTrainer(ctx, user_id)
+	if err != nil {
+		return false, err
 	}
+
+	return trainer, nil
 }
 
-func evalSelf(ctx context.Context) (bool, error) {
+func isSelf(ctx context.Context) (bool, error) {
 	fc := graphql.GetFieldContext(ctx)
 	if fc == nil {
 		return false, fmt.Errorf("Failed to get field context")
@@ -107,9 +85,61 @@ func evalSelf(ctx context.Context) (bool, error) {
 	return target_id == user_id, nil
 }
 
-func isSelf() func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-	return func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
-		self, err := evalSelf(ctx)
+func SetupDirectives(config *gql.Config, store *database.Store) {
+	config.Directives.IsAdmin = func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+		admin, err := isAdmin(store, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		if admin {
+			return next(ctx)
+		} else {
+			return nil, fmt.Errorf("Unauthorized")
+		}
+	}
+
+	config.Directives.IsManager = func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+		manager, err := isManager(store, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		if manager {
+			return next(ctx)
+		} else {
+			return nil, fmt.Errorf("Unauthorized")
+		}
+	}
+
+	config.Directives.IsStaff = func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+		staff, err := isStaff(store, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		if staff {
+			return next(ctx)
+		} else {
+			return nil, fmt.Errorf("Unauthorized")
+		}
+	}
+
+	config.Directives.IsTrainer = func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+		trainer, err := isTrainer(store, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		if trainer {
+			return next(ctx)
+		} else {
+			return nil, fmt.Errorf("Unauthorized")
+		}
+	}
+
+	config.Directives.IsSelf = func(ctx context.Context, obj any, next graphql.Resolver) (any, error) {
+		self, err := isSelf(ctx)
 		if err != nil {
 			return nil, err
 		}
@@ -120,13 +150,4 @@ func isSelf() func(ctx context.Context, obj any, next graphql.Resolver) (any, er
 			return nil, fmt.Errorf("Unauthorized")
 		}
 	}
-}
-
-func SetupDirectives(config *gql.Config, store *database.Store) {
-	config.Directives.IsAdmin = isAdmin(store)
-	config.Directives.IsManager = isManager(store)
-	config.Directives.IsStaff = isStaff(store)
-	config.Directives.IsTrainer = isTrainer(store)
-
-	config.Directives.IsSelf = isSelf()
 }
