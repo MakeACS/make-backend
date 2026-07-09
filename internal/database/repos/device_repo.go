bf3c4a46c5cae943b6ebae8a0ed3f111237fbeaa
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-07-08 23:43:58 -0400

    Added GetAccessDevice and GetAccessChannel methods

--- internal/database/repos/device_repo.go
+++ internal/database/repos/device_repo.go
@@ -82,3 +82,56 @@ func (r *DeviceRepo) GetDeviceBySN(ctx context.Context, sn string) (*models.Devi
 
 	return &equipment_result, nil
 }
+
+func (r *DeviceRepo) GetAccessDeviceById(ctx context.Context, id int) (*models.AccessDevice, error) {
+	var device_result models.AccessDevice
+
+	query := `SELECT
+		device_id,
+		channels,
+		temp_duration,
+		current_card_tag,
+		last_status,
+		session_start,
+		flags,
+		sealed_deployment,
+		reported_deployment
+		FROM access_devices WHERE device_id = $1
+	`
+
+	err := r.DB.QueryRowContext(ctx, query, id).Scan(
+		&device_result.DeviceId,
+		&device_result.Channels,
+		&device_result.TempDuration,
+		&device_result.CurrentCardTag,
+		&device_result.LastStatus,
+		&device_result.SessionStart,
+		&device_result.Flags,
+		&device_result.SealedDeployment,
+		&device_result.ReportedDeployment,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &device_result, nil
+}
+
+func (r *DeviceRepo) GetAccessChannelByDeviceAndChannelId(ctx context.Context, device_id int, channel_id int) (*models.AccessChannel, error) {
+	var access_channel_result models.AccessChannel
+
+	query := `SELECT id, device_id, channel_id, state, temp_duration FROM access_channels WHERE device_id = $1 AND channel_id = $2`
+
+	err := r.DB.QueryRowContext(ctx, query, device_id, channel_id).Scan(
+		&access_channel_result.Id,
+		&access_channel_result.DeviceId,
+		&access_channel_result.ChannelId,
+		&access_channel_result.State,
+		&access_channel_result.TempDuration,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &access_channel_result, nil
+}
